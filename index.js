9fc0f97cc5cfd14f97ca292f748ccf3501e29f71
KIMGEEK <hajunbaek025@gmail.com>
2025-01-12 16:18:06 +0900

    이름 검색 기능 추가가

--- index.js
+++ index.js
@@ -7,10 +7,42 @@ async function searchBirth() {
         await fetch("https://my-json-server.typicode.com/yanghaemi/json_placeholder/"+input_num)
         .then((response) => response.json())
         .then((data) => {
+            console.log(data);
             const html = createHTMLUserBirth(data);
             document.getElementById("result").innerHTML = html;
         });
         return;
+    } else {
+        const response = await fetch("https://my-json-server.typicode.com/yanghaemi/json_placeholder/db")
+        const data = await response.json();
+        console.log(data);
+        switch(input_num) {
+            case '양해미':  // if (input_num === '양해미')
+                result = data["1"];
+                break;
+            case '송영은':  // if (input_num === '송영은')
+                result = data["2"];
+                break;
+            case '강지웅':  // if (input_num === '강지웅')
+                result = data["3"];
+                break;
+            case '오현지':  // if (input_num === '오현지')
+                result = data["4"];
+                break;
+            case '윤정섭':  // if (input_num === '윤정섭')
+                result = data["5"];
+                break;
+            default:
+                result = [];
+                break;
+          }
+        console.log(result);
+        if(result.length === 0) {
+            alert("존재하지 않음");
+            return;
+        }
+        const html = createHTMLUserBirth(result);
+        document.getElementById("result").innerHTML = html;
     }
 }
 
