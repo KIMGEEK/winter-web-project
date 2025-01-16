d15da048f72526f8f41dbe3cfe2e6472e0a77a27
KIMGEEK <hajunbaek025@gmail.com>
2025-01-16 17:54:42 +0900

    add selected color
    if click send button, log checked color currently.

--- idea-note/idea_note.js
+++ idea-note/idea_note.js
@@ -4,4 +4,12 @@ function getColor() {
     var count = colorList.length;
 
     console.log('라디오버튼 개수', count);
+
+    for (var i = 0; i < count; i++) {
+        if (colorList[i].checked) {
+            console.log('선택된 색상', colorList[i].value);
+            return colorList[i].value;
+        }
+        
+    }
 };
