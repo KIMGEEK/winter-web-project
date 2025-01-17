154cb8b5ccc89305e3ba4dd9486ce55240ff8e76
KIMGEEK <hajunbaek025@gmail.com>
2025-01-17 17:25:06 +0900

    feat: update button feature getcolor to addmessage

--- idea-note/idea_note.js
+++ idea-note/idea_note.js
@@ -1,15 +1,36 @@
 function getColor() {
     const colorList = document.getElementsByClassName('form-check-input');
 
-    var count = colorList.length;
-
-    console.log('라디오버튼 개수', count);
-
     for (var i = 0; i < count; i++) {
         if (colorList[i].checked) {
             console.log('선택된 색상', colorList[i].value);
             return colorList[i].value;
         }
-        
     }
 };
+
+function addMessage() {
+            // Get the input element
+            const input = document.getElementById('messageInput');
+            const messageText = input.value.trim();
+
+            // Check if the message is not empty
+            if (messageText !== '') {
+                // Create a new message element
+                const messageElement = document.createElement('div');
+                messageElement.className = 'message';
+                messageElement.textContent = messageText;
+
+                // Get the message board
+                const messageBoard = document.getElementById('messageBoard');
+
+                // Add the new message at the top of the board
+                messageBoard.insertBefore(messageElement, messageBoard.firstChild);
+
+                // Clear the input field
+                input.value = '';
+            }
+
+            // Return focus to input field
+            input.focus();
+        }
