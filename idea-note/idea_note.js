85fe1ed223af5abddc8657dac3e9fdd8022f64d3
KIMGEEK <hajunbaek025@gmail.com>
2025-01-20 13:49:39 +0900

    feat: improve message display layout
    I moved the location of the delete button to the bottom of the memo.

--- idea-note/idea_note.js
+++ idea-note/idea_note.js
@@ -27,13 +27,15 @@ function addMessage() {
                         </div>
             </div>
             <div class="card-body">
-                <div class="d-flex justify-content-between align-items-top">
-                    <div class="flex-grow-1">
+                <div class="justify-content-between align-items-top">
+                    <div class="flex-grow-1 m-2">
                         <div class="message-text">${messageText}</div>
                     </div>
+                    <div class="flex-grow-1 m-1">
                     <button class="btn btn-danger btn-sm ms-2" onclick="this.closest('.card').remove()">
                         Delete
                     </button>
+                    </div>
                 </div>
             </div>
         </div>
