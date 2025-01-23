29233987754498628cbdfc22c2f742f226ff74df
KIMGEEK <hajunbaek025@gmail.com>
2025-01-23 21:26:51 +0900

    feat: implement basic layout with Header, Nav, and Article components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,12 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function Article(props) {
   return (
-    <div className="App">
-        hi, react!
+    <article>
+      <h2>{props.title}</h2>
+      {props.body}
+    </article>
+  )
+}
+function Header(props) {
+  return (
+    <header>
+      <h1><a href="/" onClick={
+        (e) => {
+          e.preventDefault();
+          props.onChangeMode();
+        }
+      }>{props.title}</a></h1>
+    </header>
+  )
+}
+function Nav(props) {
+  const lis = [
+  ];
+  for (let i = 0; i < props.topics.length; i++) {
+    let t = props.topics[i];
+    lis.push(<li key={t.id}>
+      <a id={t.id} href={"/read/"+t.id} onClick={event=>{
+        event.preventDefault();
+        props.onChangeMode(event.target.id);
+      }}>{t.title}</a></li>);
+  }
+  return (
+    <nav>
+      <ol>
+        {lis}
+      </ol>
+    </nav>
+  )
+}
+function App() {
+  const topics = [
+    {id: 1, title: 'HTML', desc: 'HTML is ...'},
+    {id: 2, title: 'CSS', desc: 'CSS is ...'},
+    {id: 3, title: 'JavaScript', desc: 'JavaScript is ...'},
+  ];
+  return (
+    <div>
+      <Header title="WEB" onChangeMode={()=>{
+        alert('header');
+      }}/>
+      <Nav topics={topics} onChangeMode={(id)=>{
+        alert(id);
+      }}/>
+      <Article title="Welcome" body="Hello, WEB!"/>
     </div>
-  );
+  )
 }
 
 export default App;
