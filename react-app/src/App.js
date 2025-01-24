cb6544cbc61f2a6438a72fd86edc04b3ad2ff8b2
KIMGEEK <hajunbaek025@gmail.com>
2025-01-24 17:04:27 +0900

    feat: enhance App component to manage topic selection and display content dynamically

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -30,7 +30,7 @@ function Nav(props) {
     lis.push(<li key={t.id}>
       <a id={t.id} href={"/read/"+t.id} onClick={event=>{
         event.preventDefault();
-        props.onChangeMode(event.target.id);
+        props.onChangeMode(Number(event.target.id));
       }}>{t.title}</a></li>);
   }
   return (
@@ -42,28 +42,35 @@ function Nav(props) {
   )
 }
 function App() {
-
   const [mode, setMode] = useState('WELCOME');
-  let content = null;
-  if(mode === 'WELCOME') {
-    content = <Article title="Welcome" body="Hello, WEB!"/>
-  }
-  else if(mode === 'READ') {
-    content = <Article title="Read" body="Hello, Read"/>
-  }
-
+  const [id, setId] = useState(null);
   const topics = [
     {id: 1, title: 'HTML', desc: 'HTML is ...'},
     {id: 2, title: 'CSS', desc: 'CSS is ...'},
     {id: 3, title: 'JavaScript', desc: 'JavaScript is ...'},
   ];
+  let content = null;
+  if(mode === 'WELCOME') {
+    content = <Article title="Welcome" body="Hello, WEB!"/>
+  }
+  else if(mode === 'READ') {
+    let title, desc = null;
+    for (let i = 0; i < topics.length; i++) {
+      if(topics[i].id === id) {
+        title = topics[i].title;
+        desc = topics[i].desc;
+      }
+    }
+    content = <Article title={title} body={desc}/>
+  }
   return (
     <div>
       <Header title="WEB" onChangeMode={()=>{
         setMode('WELCOME');
       }}/>
-      <Nav topics={topics} onChangeMode={(id)=>{
+      <Nav topics={topics} onChangeMode={(_id)=>{
         setMode('READ');
+        setId(_id);
     }}/>
       {content}
     </div>
