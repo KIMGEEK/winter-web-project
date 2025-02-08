f91e1076d17773c0cbf4aa8de88566160720232b
KIMGEEK <hajunbaek025@gmail.com>
2025-02-08 11:20:55 +0900

    feat: add calculator object with basic arithmetic functions

--- clone-react/app.js
+++ clone-react/app.js
@@ -1,6 +1,21 @@
-const siren = (word) => {
-    alert(word);
+const calculator = {
+    plus: function(a, b) {
+        console.log(a+b);
+    },
+    minus: function(a, b) {
+        console.log(a-b);
+    },
+    multiply: function(a, b) {
+        console.log(a*b);
+    },
+    divide: function(a, b) {
+        console.log(a/b);
+    },
+    mod: function(a, b) {
+        console.log(a%b);
+    },
+    pow: function(a, b) {
+        console.log(a**b);
+    },
 }
 
-siren("Hey!!!");
-siren("Haikyuu!!");
