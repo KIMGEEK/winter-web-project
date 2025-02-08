176184b8dca6aac9c6d41f962cdb5742b8fa3473
KIMGEEK <hajunbaek025@gmail.com>
2025-02-08 14:36:33 +0900

    refactor: update calculator functions to use alert instead of console.log

--- clone-react/app.js
+++ clone-react/app.js
@@ -1,21 +1,24 @@
 const calculator = {
     plus: function(a, b) {
-        console.log(a+b);
+        alert(a+b);
     },
     minus: function(a, b) {
-        console.log(a-b);
+        alert(a-b);
     },
     multiply: function(a, b) {
-        console.log(a*b);
+        alert(a*b);
     },
     divide: function(a, b) {
-        console.log(a/b);
+        alert(a/b);
     },
     mod: function(a, b) {
-        console.log(a%b);
+        alert(a%b);
     },
     pow: function(a, b) {
-        console.log(a**b);
+        alert(a**b);
     },
 }
 
+calculator.pow(2, 3);
+calculator.divide(3 , 2);
+calculator.mod(5, 3);
